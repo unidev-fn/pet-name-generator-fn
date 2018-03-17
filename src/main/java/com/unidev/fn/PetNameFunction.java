41782526cc4ea852f3792bca6e5ed5dcf3f2d271
Denis O <denis@universal-development.com>
2018-03-17 21:44:20 +0200

    Added parsing of data content

--- src/main/java/com/unidev/fn/PetNameFunction.java
+++ src/main/java/com/unidev/fn/PetNameFunction.java
@@ -1,11 +1,48 @@
 package com.unidev.fn;
 
+import java.util.HashMap;
+import java.util.Map;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import org.apache.commons.lang3.StringUtils;
+
 public class PetNameFunction {
 
-    public String handleRequest(String input) {
-        String name = (input == null || input.isEmpty()) ? "world"  : input;
+    public Map handleRequest(String input) {
 
-        return "Hello, " + name + "!";
+        try {
+            OkHttpClient client = createHttpClient();
+            Request request = createRequest("gfhfghfg");
+            Response response = client.newCall(request).execute();
+            String body = response.body().string();
+            return parseValues(body);
+        } catch (Exception e) {
+            e.printStackTrace();
+            return Map.of();
+        }
     }
+
+
+    private OkHttpClient createHttpClient() {
+        return new OkHttpClient();
+    }
+
+    private Request createRequest(String seedName) {
+        HttpUrl.Builder urlBuilder
+            = HttpUrl.parse("http://gangstaname.com/names/pet/generate").newBuilder();
+        urlBuilder.addQueryParameter("name", seedName);
+        String url = urlBuilder.build().toString();
+        Request request = new Request.Builder().url(url).build();
+        return request;
+    }
+
+    private Map<String, String> parseValues(String body) {
+        String name = StringUtils.substringBetween(body, "<h2 class='name'>", "</h2>");
+        String usage = StringUtils.substringBetween(body, "<p class='usage'>Usage: &quot;", "&quot;</p>");
+        return Map.of("name", name, "usage", usage);
+    }
+
 
 }
